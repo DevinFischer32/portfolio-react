008eac6dc4e1bbd70770ddb5bbdc22aac73baaa2
Devin Fischer <devinfischer32@gmail.com>
2021-12-01 17:09:03 -0800

    Portfolio Website

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,23 +1,70 @@
 import React from "react";
+import Linkedin from "./Logo/linkedin-logo.png";
+import github from "./Logo/github.png";
+import email from "./Logo/email.png";
+import resume from "./Logo/cv.png";
 
 export default function Contact() {
   return (
     <div id="Contact">
-      <h1 id="contact-title" className="titles">
-        Contact
-      </h1>
-      <div className="flex">
-        <div id="left">
-          <form className="column">
-            <input type="text" placeholder="name" />
-            <input type="text" placeholder="message" />
-            <input type="button" value="Submit" />
-          </form>
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+      >
+        <path
+          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+          className="shape-fill"
+          fill="#E6E8E6"
+          fillOpacity="1"
+        ></path>
+      </svg>
+      <div className="flexContact">
+        <div>
+          <a
+            href="https://www.linkedin.com/in/devin-fischer/"
+            target="_blank"
+            rel="noreferrer"
+            className="contactR"
+          >
+            <img src={Linkedin} alt="linkedin" className="contactLogo" />
+            <p className="contactLogoText">Linkedin</p>
+          </a>
         </div>
-        <div id="right">
-          <p>Linkedin</p>
-          <p>Github</p>
-          <p>Email</p>
+        <div>
+          <a
+            href="https://github.com/DevinFischer32"
+            target="_blank"
+            rel="noreferrer"
+            className="contactR"
+          >
+            <img src={github} alt="github" className="contactLogo" />
+            <p className="contactLogoText">Github</p>
+          </a>
+        </div>
+        <div>
+          <a
+            href="mailto:devinfischer.dev@gmail.com "
+            target="_blank"
+            rel="noreferrer"
+            className="contactR"
+          >
+            <img src={email} alt="email" className="contactLogo" />
+            <p className="contactLogoText">DevinFischer.dev@gmail.com</p>
+          </a>
+        </div>
+
+        <div>
+          <a
+            href="https://docs.google.com/document/d/1A1k9nEkXs9OiuFztZ0Ik-7xtdJAyTOWU2zBnq3TapjU/edit?usp=sharing "
+            target="_blank"
+            rel="noreferrer"
+            className="contactR"
+          >
+            <img src={resume} alt="resume" className="contactLogo" />
+            <p className="contactLogoText">Resume</p>
+          </a>
         </div>
       </div>
     </div>
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,39 +1,57 @@
 import React from "react";
+import reactLogo from "./Logo/atom.png";
+import jsLogo from "./Logo/Javascript-Logo.png";
+import html from "./Logo/html.png";
+import node from "./Logo/node.js.png";
+import postgres from "./Logo/postgre.png";
+import cssLogo from "./Logo/css-3.png";
+import git from "./Logo/git.png";
+import firebase from "./Logo/firebase.png";
 
 export default function Skills() {
   return (
-    <div id="Skills">
-      <h1 className="titles" id="skills-title">
-        Skills
-      </h1>
-      <div className="skills-row">
-        <div className="column">
-          <h5 className="known-titles">React</h5>
+    <>
+      <div id="Skills">
+        <h1 className="titles" id="skills-title">
+          Skills
+        </h1>
+        <div className="skills-row">
+          <div className="column">
+            <img src={reactLogo} alt="" className="skillsLogo" />
+            <h5 className="known-titles">React</h5>
+          </div>
+          <div className="column">
+            <img src={jsLogo} alt="" className="skillsLogo" />
+            <h5 className="known-titles">JavaScript</h5>
+          </div>
+          <div className="column">
+            <img src={node} alt="" className="skillsLogo" />
+            <h5 className="known-titles">Node.js</h5>
+          </div>
+          <div className="column">
+            <img src={postgres} alt="" className="skillsLogo" />
+            <h5 className="known-titles">PostgreSQL</h5>
+          </div>
         </div>
-        <div className="column">
-          <h5 className="known-titles">JavaScript</h5>
-        </div>
-        <div className="column">
-          <h5 className="known-titles">Node.js</h5>
-        </div>
-        <div className="column">
-          <h5 className="known-titles">PostgreSQL</h5>
+        <div className="skills-row">
+          <div className="column">
+            <img src={html} alt="" className="skillsLogo" />
+            <h5 className="known-titles">HTML</h5>
+          </div>
+          <div className="column">
+            <img src={cssLogo} alt="" className="skillsLogo" />
+            <h5 className="known-titles">CSS</h5>
+          </div>
+          <div className="column">
+            <img src={git} alt="" className="skillsLogo" />
+            <h5 className="known-titles">Git</h5>
+          </div>
+          <div className="column">
+            <img src={firebase} alt="" className="skillsLogo" />
+            <h5 className="known-titles">Firebase</h5>
+          </div>
         </div>
       </div>
-      <div className="skills-row">
-        <div className="column">
-          <h5 className="known-titles">HTML</h5>
-        </div>
-        <div className="column">
-          <h5 className="known-titles">CSS</h5>
-        </div>
-        <div className="column">
-          <h5 className="known-titles">Git</h5>
-        </div>
-        <div className="column">
-          <h5 className="known-titles">Github</h5>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
